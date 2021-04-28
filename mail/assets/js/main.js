db4a4b33e36717deec0fba4de20f11908ac98b92
evelingcastillon <evelingcastillon0304@gmail.com>
2021-04-29 01:52:26 +0200

    completato mail con ciclo for

--- mail/assets/js/main.js
+++ mail/assets/js/main.js
@@ -3,8 +3,27 @@
 var iscritti = ["eveling@gmail.com" , "ricardo@gmail.com" , "alice@gmail.com"]
 var emailInserita = prompt("Inserire email")
 
-if((emailInserita == iscritti[0]) || (emailInserita == iscritti[1]) || (emailInserita == iscritti[2])) {
+/* if((emailInserita == iscritti[0]) || (emailInserita == iscritti[1]) || (emailInserita == iscritti[2])) {
     console.log("Email verificata");
 } else {
     console.log("Email non verificata");
+} */
+
+var invitato = false
+
+for(var i = 0; i < iscritti.length; i++) {
+    var email = iscritti[i]
+    if(emailInserita == email) {
+        invitato = true   
+    }
+}
+
+var message;
+if (invitato) {
+    message = "Email verificata"
+    console.log(message);
+} else {
+    message = "Email non valida"
+    console.log(message);
+
 }
